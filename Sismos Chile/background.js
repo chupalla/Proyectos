7e093fb763aa9d62f7746b0b742cd89013032bc5
Andres Puebla Lastarria <andrespuebla32@gmail.com>
2020-09-17 10:14:26 +0200

    Add files via upload

--- Sismos Chile/background.js
+++ Sismos Chile/background.js
@@ -1,8 +1,8 @@
 chrome.contextMenus.create({
-	contexts: ['browser_action'],
-    title: 'Haz una Donación',
-    onclick: function(e){
-		var newURL = "https://www.flow.cl/btn.php?token=2lbi11r";
-		chrome.tabs.create({ url: newURL });
-    }
-}, function(){})
+  contexts: ['browser_action'],
+  title: 'Haz una Donación',
+  onclick: function (e) {
+    var newURL = "https://www.flow.cl/btn.php?token=2lbi11r";
+    chrome.tabs.create({ url: newURL });
+  }
+}, function () { })
