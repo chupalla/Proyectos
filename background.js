c5f8eb9284a58770c6f6889891ee6e33f33df5fe
Andres Puebla Lastarria <andrespuebla32@gmail.com>
2020-09-17 12:17:23 +0200

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.contextMenus.create({
-  contexts: ['browser_action'],
-  title: 'Haz una Donación',
-  onclick: function (e) {
-    var newURL = "https://www.flow.cl/btn.php?token=2lbi11r";
-    chrome.tabs.create({ url: newURL });
-  }
-}, function () { })
